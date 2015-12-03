92c7c7fe729189fcd260053ca8d54afd212b0e31
leBoer <christianborre@gmail.com>
2015-12-03 20:54:48 +0100

    Add jobs and projects objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,39 +1,87 @@
 
-var name = "Christian";
-var formattedName =
-  HTMLheaderName.replace("%data%", name);
+// var name = "Christian";
+// var formattedName =
+//   HTMLheaderName.replace("%data%", name);
 
-var role = "Web Developer";
-var formattedRole =
-  HTMLheaderRole.replace("%data%", role);
+// var role = "Web Developer";
+// var formattedRole =
+//   HTMLheaderRole.replace("%data%", role);
 
-var skills = ["html", "css", "javascript","Git"];
-var formattedSkills =
-  HTMLskills.replace("%data%", skills);
+// var skills = ["html", "css", "javascript","Git"];
+// var formattedSkills =
+//   HTMLskills.replace("%data%", skills);
 
-var email = "Christianborre@gmail.com";
-var formattedEmail =
-  HTMLemail.replace("%data%", email);
+// var email = "Christianborre@gmail.com";
+// var formattedEmail =
+//   HTMLemail.replace("%data%", email);
 
-var mobile = "+47 977 80 867";
-var formattedMobile =
-  HTMLmobile.replace("%data%", mobile);
+// var mobile = "+47 977 80 867";
+// var formattedMobile =
+//   HTMLmobile.replace("%data%", mobile);
 
-var welcome = "Welcome to my resume. I am pleased to have you here. Have a look around and let me know if I can help you";
-var formattedWelcome =
-  HTMLwelcomeMsg.replace("%data%", welcome);
+// var welcome = "Welcome to my resume. I am pleased to have you here. Have a look around and let me know if I can help you";
+// var formattedWelcome =
+//   HTMLwelcomeMsg.replace("%data%", welcome);
 
-var bioPic = "images/me.jpg";
-var formattedbioPic =
-  HTMLbioPic.replace("%data%", bioPic);
+// var bioPic = "images/me.jpg";
+// var formattedbioPic =
+//   HTMLbioPic.replace("%data%", bioPic);
 
-//$("#header").prepend(formattedRole);
-//$("#header").prepend(formattedName);
-//$("#header").append(formattedSkills);
-//$("#header").append(formattedEmail);
-//$("#header").append(formattedMobile);
-//$("#header").append(formattedWelcome);
+// $("#header").prepend(formattedRole);
+// $("#header").prepend(formattedName);
+// $("#header").append(formattedSkills);
+// $("#header").append(formattedEmail);
+// $("#header").append(formattedMobile);
+// $("#header").append(formattedWelcome);
+var work = {
+  "jobs": [
+  {
+    "employer": "Teekay",
+    "title": "Project Engineer",
+    "location": "Stavanger, Norway",
+    "dates": "2011 - Present",
+    "description": "Various tasks in the shipping company, mainly responsible for Maintenance Management."
+  },
+  {
+    "employer": "Transocean",
+    "title": "Intern",
+    "location": "The North Sea",
+    "dates": "Summer of 2010",
+    "description": "Summer internship on an offshore drilling rig in the North Sea."
+  }
+  ]
+};
 
+var projects = {
+  "projects": [
+  {
+    "title": "Online Resume",
+    "dates": "December 2015",
+    "description": "Creating an online resume as part or the Udacity Nanodegree. Includes Javascript",
+    "images": [
+    {
+      "resumeimg1" = "images/197x148.gif"
+    },
+    {
+      "resumeimg2" = "images/197x148.gif"
+    }
+    ]
+  },
+  {
+    "title": "Future Project",
+    "dates": "January 2016",
+    "description": "A future project that I'll be working on in the future. Very futuristic.",
+    "images": [
+    {
+      "futurepic1" = "images/197x148.gif"
+    },
+    {
+      "futurepic2" = "images/197x148.gif"
+    }
+    ]
+  }
+  ]
+};
 
 var bio = {
   "name" : formattedName,
@@ -50,10 +98,21 @@ var bio = {
   "image" : formattedbioPic
 };
 
-var work = {
-};
-
 var education = {
+  "schools": [
+  {
+    "name": "NTNU",
+    "city": "Trondheim",
+    "degree": "Masters",
+    "major": ["Marine Technology"]
+  },
+  {
+    "name": "Nanyang",
+    "city": "Singapore",
+    "degree": "Under Grad Subjects",
+    "major": ["Marine Technology"]
+  }
+  ]
 };
 
 work.position = "Engineer";
@@ -67,12 +126,12 @@ education["city"] = "Trondheim";
 
 bio["email"] = formattedEmail;
 
-$("#header").append(work["position"]);
-$("#header").append(education.name);
-$("#header").append(bio.name);
-$("#header").append(bio.role);
-$("#header").append(bio.email);
-$("#header").append(bio.mobile);
-$("#header").append(bio.image);
-$("#header").append(bio.welcomeMessage);
-$("#header").append(bio.skills);
+// $("#header").append(work["position"]);
+// $("#header").append(education.name);
+// $("#header").append(bio.name);
+// $("#header").append(bio.role);
+// $("#header").append(bio.email);
+// $("#header").append(bio.mobile);
+// $("#header").append(bio.image);
+// $("#header").append(bio.welcomeMessage);
+// $("#header").append(bio.skills);
